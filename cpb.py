d6cad341b60cecfe32c5c9ad9541bad8be7c252d
Runamook <tothe8c@gmail.com>
2017-12-05 00:40:21 +0300

    Update cpb.py

--- cpb.py
+++ cpb.py
@@ -4,37 +4,44 @@
 
 from zeep import Client
 import datetime
+import codecs
+import os
 
 
 def get_data(bank, required_time, url):
 
-    #print "\n\n Bank %s \n\n" % bank
     client = Client(url)
     result = client.service.Data135FormFullXML(bank, required_time)
+    header = "Year;V;C"
+    filename = bank + '.xlsx'
 
-    for i in result.xpath('F135_3'):
-        V = i.xpath('V3')
-        C = i.xpath('C3')
-        if len(V) == 0:
-            pass
-            #return "C = %s" % (C[0].text)
-        else:
-            #return "C = %s, V = %s" % (C[0].text, V[0].text)
-            print "%s;%s;%s" % (required_time, V[0].text, C[0].text)
-            #print (C[0].text, V[0].text)
+    # Check if file already exists. If yes - append to it, otherwise - create a new one
 
-# >>> print res.xpath('F135_3/C3')[0].text
-# Н1
-# >>> print res.xpath('F135_3/V3')[0].text
-# 10.540
+    if os.path.exists(filename):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+
+    with codecs.open(filename, append_write,'utf-8') as ofile:
+
+        for i in result.xpath('F135_3'):
+            V = i.xpath('V3')
+            C = i.xpath('C3')
+
+            if len(V) == 0:
+                pass
+            else:
+            
+                line = "%s;%s;%s" % (required_time, V[0].text, C[0].text)
+                ofile.write(line + '\n')
+                print line
 
 def main():
     
     url = 'http://www.cbr.ru/CreditInfoWebServ/CreditOrgInfo.asmx?WSDL'
     bank = '1481'
     
-    print "Year;V;C"
-    start_year = 2013
+    start_year = 2011
     end_year = 2017
 
     day = 1
@@ -48,14 +55,9 @@ def main():
             required_time = datetime.date(year, month, day)
     
             get_data(bank, required_time, url)
-            #(norm, value) = get_data(bank, required_time, url)
-            #print "%s,%s,%s" % (required_time, norm, value)
-
 
     # bank_list = ['1481'] 
     # 1481 - Sberbank
-    
-    # End of variables
     
 if __name__ == "__main__":
     main()
